9478790079d7fa0b2603a4d8cae190c1c7c98362
Louis Cahyadi <louiscahyadi23@gmail.com>
2018-03-31 23:59:22 +0700

    Add files via upload

---
+++ typetubes.pas
@@ -0,0 +1,61 @@
+unit typetubes;
+
+interface
+
+const
+	arrMin = 1;
+	arrMax = 20;
+
+type
+	daftarBahan = record
+		NamaBahan			: string;
+		HargaSatuan			: longint;
+		DurasiKadaluarsa	: integer;
+	end;
+	
+	tabDaftarBahan = record
+		tab		: array [arrMin..arrMax] of daftarBahan;
+		neff	: integer;
+	end;
+	
+	daftarResep = record
+		NamaResep			: string;
+		HargaJual			: longint;
+		JumlahBahan			: integer;
+		Bahan				: array [1..20] of string; //GTW BENER GA INI
+	end;
+	
+	tabDaftarResep = record
+		tab		: array [arrMin..arrMax] of daftarResep;
+		neff	: integer;
+	end;
+	
+	daftarInventori = record
+		NamaBahanAtauOlahan	: string;
+		DurasiKadaluarsa	: integer;
+	end;
+	
+	tabDaftarInventori = record
+		tab		: array [arrMin..arrMax] of daftarInventori;
+		neff	: integer;
+	end;
+	
+	statusPengguna = record
+		JumlahEnergi			: integer;
+		JumlahHariHidup			: integer;
+		KapasitasMaxInventori	: integer;
+		JumlahBahanBeli			: integer;
+		JumlahResepMasak		: integer;
+		JumlahMakananKadaluarsa	: integer;
+		TotalPemasukan			: longint;
+		TotalPengeluaran		: longint;
+	end;
+	
+	tabStatusPengguna = record
+		tab		: array [arrMin..arrMax] of statusPengguna;
+		neff	: integer;
+	end;
+	
+implementation
+
+end.
